d62abb40bc694d0cf3c4d2eb7b5e1b2843108e67
doolybom1 <elqkqkwkd@gmail.com>
2020-02-11 13:46:59 +0900

    simple memo

---
+++ Doit/app/src/main/java/com/biz/doit/MainActivity.java
@@ -0,0 +1,19 @@
+package com.biz.doit;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onButton1Clicked(View view) {
+    }
+}
